02badd86190074d181e73f16251a5c7a80ad5b53
Williams Gomes <williamsgomess@outlook.com>
2020-06-23 00:08:56 -0300

    Estrutura e padrões
    Debugando NodeJS

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,10 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ message: 'Hello World' }));
+app.get('/', (req, res) => {
+  const nome = 'Williams';
+  return res.json({ message: 'Hello World' });
+});
 
 app.listen(3333, () => {
   console.log('🚀️ Server started on port 3333!');
