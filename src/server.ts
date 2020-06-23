6b46a76c863b47289221063ee25f35587279d477
Williams Gomes <williamsgomess@outlook.com>
2020-06-22 23:57:11 -0300

    Estrutura e padrôes
    Padrões de Projeto com Eslint, Prettier e Editor Config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' })
-})
+app.get('/', (req, res) => res.json({ message: 'Hello World' }));
 
 app.listen(3333, () => {
   console.log('🚀️ Server started on port 3333!');
