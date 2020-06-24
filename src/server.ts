4f140952d2b283557fa7bccd6212132114b8f670
Williams Gomes <williamsgomess@outlook.com>
2020-06-23 23:40:39 -0300

    Construindo Aplicação
    Validando a data

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,10 @@ import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+app.use(routes);
+
 app.get('/', (req, res) => {
-  const nome = 'Williams';
   return res.json({ message: 'Hello World' });
 });
 
