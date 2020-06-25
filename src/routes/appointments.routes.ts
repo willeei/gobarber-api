97a994f06ec3ef7291d9542d5b4cbb136614ba2e
Williams Gomes <williamsgomess@outlook.com>
2020-06-24 22:07:25 -0300

    Cadastro de usuários
    Criação de registro

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -15,18 +15,18 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
 
     const createAppointmentService = new CreateAppointmentService();
 
     const appointment = await createAppointmentService.execute({
-      provider,
+      provider_id,
       date: parsedDate,
     });
 
-    return response.json(appointment);
+    return response.status(201).json(appointment);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
